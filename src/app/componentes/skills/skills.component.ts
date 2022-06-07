1530cfb3e2fe81abd44c8b91a656ccd3c15310d0
PyroC4t <tiburopunk@gmail.com>
2022-06-07 18:47:02 -0300

    implementando datos a traves de JSON

--- src/app/componentes/skills/skills.component.ts
+++ src/app/componentes/skills/skills.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
 
 @Component({
   selector: 'app-skills',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skills.component.css']
 })
 export class SkillsComponent implements OnInit {
+  skills:any;
+  skillsList:any;
 
-  constructor() { }
+  constructor(private datosPortfolio:PortfolioService) { }
 
   ngOnInit(): void {
+    this.datosPortfolio.obtenerDatos().subscribe(data => {
+      console.log(data);
+      this.skillsList = data.skills;
+    })
   }
 
 }
